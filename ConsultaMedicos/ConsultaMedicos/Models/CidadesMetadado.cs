056a79a153b6e297ef3c97ff9200495dbe73edb3
Ana Lucia Rizzi <ana.rizzi.eu@gmail.com>
2020-03-27 18:28:17 -0300

    Projeto de cadastro e consulta a médicos em MVC utilizando banco de dado SQL Server, desenvolvido de acordo com o livro da Casa do Código

---
+++ ConsultaMedicos/ConsultaMedicos/Models/CidadesMetadado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ConsultaMedicos.Models
+{
+    [MetadataType(typeof(CidadesMetadado))]
+    public partial class Cidades
+    {
+    }
+
+    public class CidadesMetadado
+    {
+        [Required(ErrorMessage ="Favor informar o nome")]
+        [StringLength(100, ErrorMessage ="O nome não pode ultrapassar 100 caracteres")]
+        public string Nome { get; set; }
+    }
+}
