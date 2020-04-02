2ab0b71b6763c0aa1a1525f18c2ec8213bd642ca
Ana Lucia Rizzi <ana.rizzi.eu@gmail.com>
2020-04-02 17:21:40 -0300

    Continuação do projeto do livro Asp.Net MVC 5, para estudos iniciais de MVC

---
+++ Teste_Inicial/Teste_Inicial/Contexts/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Teste_Inicial.Models;
+using System.Data.Entity;
+
+namespace Teste_Inicial.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
